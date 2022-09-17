1732fc3bb292986bf97a11d3de31a2d0945e4883
Чеботарь Игорь Евгеньевич <giborg@inbox.ru>
2022-09-17 17:55:39 +0300

    init

---
+++ src/main/java/ru/rutmiit/user_survey_api/UserSurveyApiApplication.java
@@ -0,0 +1,13 @@
+package ru.rutmiit.user_survey_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSurveyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSurveyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rutmiit/user_survey_api/UserSurveyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rutmiit.user_survey_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSurveyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
