e6e96d5807b2f5e6c1964eb60151625c5b16e830
Чеботарь Игорь Евгеньевич <giborg@inbox.ru>
2022-09-23 21:46:24 +0300

    add test template

---
+++ src/test/java/integration/IntegrationTest.java
@@ -0,0 +1,20 @@
+package integration;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public @interface IntegrationTest {
+}
---
+++ src/test/java/integration/RunTestInContainer.java
@@ -0,0 +1,20 @@
+package integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class RunTestInContainer { // запускает тестовый контейнер Postgresql в докере
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:alpine");
+
+    @BeforeAll
+    static void runContainer(){
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+    }
+}
